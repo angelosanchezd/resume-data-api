b2b80320c579365e9392ee4885a32c8d2be2b1ad
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-12-08 13:20:26 -0700

    changes werent merged?

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Capstone.create!([
-  {student_id: 1, name: "Test", description: "Test", URL: "test", screenshot: "test"},
-  {student_id: 1, name: "Test2", description: "Test2", URL: "test", screenshot: "test"},
-  {student_id: 1, name: "Test3", description: "Test3", URL: "test", screenshot: "test"}
-])
