01bf127903f82e96bd8aae59db59fc45567e55fb
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-12-08 12:33:27 -0700

    adds Capstone model, controller, routes, requests

---
+++ app/controllers/capstones_controller.rb
@@ -0,0 +1,42 @@
+class CapstonesController < ApplicationController
+  def index
+    capstones = Capstone.where(student_id: params[:student_id])
+    render json: capstones
+  end
+  def show
+    capstone = Capstone.find_by(id: params[:id])
+    render json: capstone
+  end
+  def create 
+    capstone = Capstone.new(
+      student_id: params[:student_id],
+      name: params[:name],
+      description: params[:description],
+      URL: params[:URL],
+      screenshot: params[:screenshot]
+    )
+    if capstone.save
+      render json: { message: "Capstone created successfully" }, status: :created
+    else
+      render json: { errors: capstone.errors.full_messages }, status: :bad_request
+    end
+  end
+  def update
+    capstone = Capstone.find_by(id: params[:id])
+    capstone.student_id = params[:student_id] || capstone.student_id
+    capstone.name = params[:name] || capstone.name
+    capstone.description = params[:description] || capstone.description
+    capstone.URL = params[:URL] || capstone.URL
+    capstone.screenshot = params[:screenshot] || capstone.screenshot
+    if capstone.save
+      render json: capstone
+    else
+      render json: { errors: capstone.errors.full_messages }, status: :bad_request
+    end
+  end
+  def destroy
+    capstone = Capstone.find_by(id: params[:id])
+    capstone.destroy
+    render json: { message: "Capstone Deleted"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/capstones/:student_id" => "capstones#index"
+  get "/capstones/show/:id" => "capstones#show"
+  post "/capstones" => "capstones#create"
+  patch "/capstones/:id" => "capstones#update"
+  delete "/capstones/:id" => "capstones#destroy"
 end
