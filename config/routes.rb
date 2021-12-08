2ec3d9882c3323788a590b8acdaab9f95601c1df
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-08 13:34:48 -0600

    adds REST routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/education/:student_id" => "educations#index"
+  get "/education/:id" => "educations#show"
+  post "/education" => "educations#create"
+  patch "/education/:id" => "educations#update"
+  delete "/education/:id" => "educations#destroy"
 end
