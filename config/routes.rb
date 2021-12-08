90a52d921aaaa98ab101ad4db7466b72ea7f418f
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-08 10:47:21 -0800

    Adds routes and finishes controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Skill Routes
+  get "/skills/:student_id" => "skills#index"
+  get "/skills/:id" => "skills#show"
+  post "/skills" => "skills#create"
+  patch "/skills/:id" => "skills#update"
+  delete "/skills/:id" => "skills#destroy"
 end
