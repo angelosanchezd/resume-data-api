292b6962b1b0bc4d49797d27aa1c0c340fb8b653
Mike Miller <mikemille999@gmail.com>
2021-12-08 14:11:36 -0600

    fix post to patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/experiences/:student_id" => "experiences#index"
   get "/experience/:id" => "experiences#show"
-  post "experiences/create" => "experiences#create"
-  post "experiences/update/:id" => "experiences#update"
-  delete "experiences/delete/:id" => "experiences#delete"
+  post "/experiences/create" => "experiences#create"
+  patch "/experiences/update/:id" => "experiences#update"
+  delete "/experiences/delete/:id" => "experiences#delete"
 end
