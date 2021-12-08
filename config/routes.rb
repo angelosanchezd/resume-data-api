dccd80c35f416a8a26430a3ffdf0b5adec3a356b
Angelo Sanchez-De Anda <angelomsd818@gmail.com>
2021-12-08 11:01:13 -0800

    Requests work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/students" => "students#index"
+
+  get "/students/:id" => "students#show"
+
+  post "/students" => "students#create"
+
+  patch "/students/:id" => "students#update"
+
+  delete "/students/:id" => "students#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+student = Student.new(
+"first_name": "Ismail",
+"last_name": "Nicholson",
+"email": "mike@example.com",
+"phone_number": "555-555-5555",
+"short_bio": "I'm me.",
+"linkedin": "mitchel@linkedin.net",
+"twitter": "mitchel@twitter.net",
+"website": "mitchel@example.gov",
+"online_resume": "Work.pdf",
+"github": "mike@github.net",
+"photo": "me.jpg"
+)
+student.save
