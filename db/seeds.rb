0a039572db35b8f510988a78ceee0762fba6e6d8
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-08 13:53:25 -0800

    Comments out seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-
-# Experience.create!([{student_id: 1, start_date: '31/12/2020', end_date: '1/1/2021', job_title: 'custodian', company: 'immercom', details: 'custodian'},{student_id: 1, start_date: '31/12/2019', end_date: '1/1/2020', job_title: 'janitor', company: 'Bank of America', details: 'Janitor'}])
-
-# Education.create!([
-#   {student_id: 1, start_date: "nov 2", end_date: "nov 3", degree: "degree", university_name: "university", details: "details"}
-# ])
-
 # Student.create!(
 #   "first_name": "",
 #   "last_name": "",
@@ -18,6 +11,12 @@
 #   "github": "",
 #   "photo": ""
 # )
+
+# Experience.create!([{student_id: 1, start_date: '31/12/2020', end_date: '1/1/2021', job_title: 'custodian', company: 'immercom', details: 'custodian'},{student_id: 1, start_date: '31/12/2019', end_date: '1/1/2020', job_title: 'janitor', company: 'Bank of America', details: 'Janitor'}])
+
+# Education.create!([
+#   {student_id: 1, start_date: "nov 2", end_date: "nov 3", degree: "degree", university_name: "university", details: "details"}
+# ])
 
 
 # Skill.create!([
