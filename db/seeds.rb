d8793b8725c4cb84adc3d3326f930b851f29f995
Mike Miller <mikemille999@gmail.com>
2021-12-08 14:42:27 -0600

    adds seeds to experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@
 Education.create!([
   {student_id: 1, start_date: "nov 2", end_date: "nov 3", degree: "degree", university_name: "university", details: "details"}
 ])
+Experience.create!([{student_id: 1, start_date: '31/12/2020', end_date: '1/1/2021', job_title: 'custodian', company: 'immercom', details: 'custodian'},{student_id: 1, start_date: '31/12/2019', end_date: '1/1/2020', job_title: 'janitor', company: 'Bank of America', details: 'Janitor'}])
