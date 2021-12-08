3c081c4fc5f82f40040337158e78664f5c378d72
Angelo Sanchez-De Anda <angelomsd818@gmail.com>
2021-12-08 12:38:20 -0800

    added student create to seeds, bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
+# Education.create!([
+#   {student_id: 1, start_date: "nov 2", end_date: "nov 3", degree: "degree", university_name: "university", details: "details"}
+# ])
 
-Education.create!([
-  {student_id: 1, start_date: "nov 2", end_date: "nov 3", degree: "degree", university_name: "university", details: "details"}
-])
+# Student.create!(
+#   "first_name": "",
+#   "last_name": "",
+#   "email": "",
+#   "phone_number": "",
+#   "short_bio": "",
+#   "linkedin": "",
+#   "twitter": "",
+#   "website": "",
+#   "online_resume": "",
+#   "github": "",
+#   "photo": ""
+# )
