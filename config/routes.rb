8b9ca54db8f2c1ac590c00f81c74afc37cc6a066
Angelo Sanchez-De Anda <angelomsd818@gmail.com>
2021-12-08 11:50:41 -0800

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Capstone Routes
   get "/capstones/:student_id" => "capstones#index"
   get "/capstones/show/:id" => "capstones#show"
   post "/capstones" => "capstones#create"
@@ -7,17 +8,10 @@ Rails.application.routes.draw do
 
   # Students Routes
   get "/students" => "students#index"
-
   get "/students/:id" => "students#show"
-
   post "/students" => "students#create"
-
   patch "/students/:id" => "students#update"
-
   delete "/students/:id" => "students#destroy"
-end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Skill Routes
   get "/skills/:student_id" => "skills#index"
@@ -25,6 +19,8 @@ end
   post "/skills" => "skills#create"
   patch "/skills/:id" => "skills#update"
   delete "/skills/:id" => "skills#destroy"
+
+  # Experiences Routes
   get "/experiences/:student_id" => "experiences#index"
   get "/experience/:id" => "experiences#show"
   post "experiences/create" => "experiences#create"
